aab17bae2441823614a15de265ca52d53395954c
uhvcell <uhvcell@nbkrist.org>
2026-05-28 14:07:36 +0530

    Add files via upload

--- posters-data.js
+++ posters-data.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Posters Data - Shared between Posters page and Home page slider
  * Add or remove images here to update both pages automatically.
@@ -23,3 +24,30 @@ const posterImages = [
         url: "poster6.jpg"
     }
 ];
+=======
+/**
+ * Posters Data - Shared between Posters page and Home page slider
+ * Add or remove images here to update both pages automatically.
+ */
+
+const posterImages = [
+    {
+        url: "poster1.jpg"
+    },
+    {
+        url: "poster2.jpg"
+    },
+    {
+        url: "poster3.jpg"
+    },
+    {
+        url: "poster4.jpg"
+    },
+    {
+        url: "poster5.jpg"
+    },
+    {
+        url: "poster6.jpg"
+    }
+];
+>>>>>>> b006b85e07726b7c3e72c3ee1cf1b0df213ec312
